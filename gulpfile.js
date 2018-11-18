e1328e0238649aa0aee2d4dc5bb8bac9fd6dc133
Roman Averkin <romaaverkin@gmail.com>
2018-11-18 15:29:21 +0700

    In works

---
+++ gulpfile.js
@@ -0,0 +1,43 @@
+var gulp         = require('gulp');
+var browserSync  = require('browser-sync');
+var sass         = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
+var concatCSS    = require('gulp-concat-css');
+var ftp          = require('gulp-ftp');
+
+// Static Server + watching scss/html files
+gulp.task('serve', ['sass'], function() {
+
+    browserSync.init({
+        server: "src/"
+    });
+    //Следим за изменениями файлов
+    gulp.watch("src/sass/*.sass", ['sass']);
+    gulp.watch("src/*.html").on('change', browserSync.reload);
+});
+
+// Компилируем Sass в SCC и обновляем страницу
+gulp.task('sass', function() {
+    return gulp.src("src/sass/**/*.sass")
+        .pipe(sass().on('error', sass.logError))
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+        }))
+        .pipe(concatCSS('main.css'))
+        .pipe(gulp.dest("src/css"))
+        .pipe(browserSync.stream());
+});
+
+gulp.task('ftp', function () {
+    return gulp.src('src/**')
+        .pipe(ftp({
+            host: 'website.com',
+            user: 'johndoe',
+            pass: '1234',
+            remotePath: 'www/averkin.ru/stream'
+        }))
+        .pipe(gutil.noop());
+});
+
+gulp.task('default', ['serve']);
